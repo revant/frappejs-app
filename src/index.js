4808df06800853ee2fab4b348b5b352924b1a29c
Revant Nandgaonkar <revant.one@gmail.com>
2018-03-25 22:13:47 +0530

    Register Models, add menu

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const client = require('frappejs/client');
 const user = require('frappejs/models/doctype/User/User.js');
 const frappe = require('frappejs');
+const menu = require('frappejs/client/desk/menu');
 
 // start server
 client.start({
     columns: 3,
     server: 'localhost:8000',
 }).then(() => {
-    frappe.desk.addSidebarItem('Home', '#');
-    frappe.desk.addSidebarItem('New User', '#new/User');
+    frappe.registerModels(require('../models'), 'client');
 
-    frappe.router.default = '#list/User';
+    frappe.desk.menu.addItem('User', '#list/User');
+    frappe.desk.menu.addItem('Session', '#list/Session');
+    frappe.desk.menu.addItem('OAuthClient', '#list/OAuthClient');
+
+    frappe.router.default = '#list/Session';
     frappe.router.show(window.location.hash);
 });
