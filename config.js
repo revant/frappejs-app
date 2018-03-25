009c5046198f9dc99d7931e1f2ddca37f7316762
Revant Nandgaonkar <revant.one@gmail.com>
2018-03-25 21:53:01 +0530

    expires ms in config

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ config.session = {};
 config.session.secret = "cats";
 config.session.cookieSecret = "cats";
 config.session.cookieMaxAge = 86400000;
+config.session.expires = 3600000;
 
 module.exports = config;
